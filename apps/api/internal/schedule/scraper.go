05bf67bf234af7ecb513f3d71a034ce29c9f4d04
haze <92948735+hazetwo@users.noreply.github.com>
2026-05-04 09:46:10 +0200

    refactor: update scraper methods to use maps for IDs and names, adjust schedule parsing

--- apps/api/internal/schedule/scraper.go
+++ apps/api/internal/schedule/scraper.go
@@ -1,10 +1,9 @@
-package scraper
+package schedule
 
 import (
 	"errors"
 	"log/slog"
 	"net/url"
-	"uz-plan-api/internal/schedule"
 
 	"github.com/gocolly/colly"
 )
@@ -20,36 +19,38 @@ func New(domain string) Scraper {
 	}
 }
 
-func (s Scraper) GetIdsOfFields(site string) []string {
-	var ids []string
+func (s Scraper) GetFields(site string) map[string]string {
+	f := make(map[string]string)
 
 	s.c.OnHTML("ul.lista-grup a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		u, err := url.Parse(link)
+		l := e.Attr("href")
+		n := e.Text
+		u, err := url.Parse(l)
 		if err != nil {
 			return
 		}
 		id := u.Query().Get("ID")
-		ids = append(ids, id)
+		f[id] = n
 	})
 
 	if err := s.c.Visit(site); err != nil {
 		slog.Warn("Failed to visit: %v", site)
 	}
-	return ids
+	return f
 }
 
-func (s Scraper) GetGroupsFromId(site string, id string) []string {
-	var ids []string
+func (s Scraper) GetGroupsFromID(site string, id string) map[string]string {
+	f := make(map[string]string)
 
 	s.c.OnHTML("table a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		u, err := url.Parse(link)
+		l := e.Attr("href")
+		n := e.Text
+		u, err := url.Parse(l)
 		if err != nil {
 			return
 		}
 		id := u.Query().Get("ID")
-		ids = append(ids, id)
+		f[id] = n
 	})
 
 	u, _ := url.Parse(site)
@@ -59,11 +60,11 @@ func (s Scraper) GetGroupsFromId(site string, id string) []string {
 	if err := s.c.Visit(u.String()); err != nil {
 		slog.Warn("Failed to visit: %v", site)
 	}
-	return ids
+	return f
 }
 
-func (s Scraper) GetScheduleForId(site string, id string) ([]schedule.Entry, error) {
-	var entries []schedule.Entry
+func (s Scraper) GetScheduleForId(site string, id string) ([]Entry, error) {
+	var entries []Entry
 	var errs []error
 
 	s.c.OnHTML("#table_details tr:has(td)", func(e *colly.HTMLElement) {
@@ -76,7 +77,7 @@ func (s Scraper) GetScheduleForId(site string, id string) ([]schedule.Entry, err
 		ClassType := e.ChildText("td:nth-child(7)")
 		teacher := e.ChildText("td:nth-child(8)")
 		classroom := e.ChildText("td:nth-child(9)")
-		ent, err := schedule.FromScraper(schedule.RawEntry{
+		ent, err := FromScraper(RawEntry{
 			Group:     group,
 			Start:     start,
 			End:       end,
@@ -90,7 +91,6 @@ func (s Scraper) GetScheduleForId(site string, id string) ([]schedule.Entry, err
 			errs = append(errs, err)
 		}
 		entries = append(entries, ent)
-
 	})
 
 	u, _ := url.Parse(site)
@@ -98,7 +98,7 @@ func (s Scraper) GetScheduleForId(site string, id string) ([]schedule.Entry, err
 	q.Set("ID", id)
 	u.RawQuery = q.Encode()
 	if err := s.c.Visit(u.String()); err != nil {
-		slog.Warn("Failed to visit: %v", site)
+		slog.Warn("Failed to visit", site)
 	}
 
 	if len(errs) > 0 {
