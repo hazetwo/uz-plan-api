d9fad143f78fa965aafaa7472b7e1bca759c8b38
haze <92948735+hazetwo@users.noreply.github.com>
2026-05-04 15:33:58 +0200

    refactor: replace colly with goquery, simplify request handling and HTML parsing

--- apps/api/internal/schedule/scraper.go
+++ apps/api/internal/schedule/scraper.go
@@ -2,108 +2,149 @@ package schedule
 
 import (
 	"errors"
-	"log/slog"
+	"log"
+	"net/http"
 	"net/url"
+	"strings"
 
-	"github.com/gocolly/colly"
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Scraper struct {
-	c *colly.Collector
 }
 
-func NewScraper(domain string) *Scraper {
-	return &Scraper{
-		c: colly.NewCollector(
-			colly.AllowedDomains(domain)),
+func NewScraper() *Scraper {
+	return &Scraper{}
+}
+
+func getDocument(site string) (*goquery.Document, error) {
+	res, err := http.Get(site)
+	if err != nil {
+		return nil, err
 	}
-}
+	defer res.Body.Close()
 
-func (s Scraper) GetFields(site string) map[string]string {
-	f := make(map[string]string)
-
-	s.c.OnHTML("ul.lista-grup a[href]", func(e *colly.HTMLElement) {
-		l := e.Attr("href")
-		n := e.Text
-		u, err := url.Parse(l)
-		if err != nil {
-			return
-		}
-		id := u.Query().Get("ID")
-		f[id] = n
-	})
-
-	if err := s.c.Visit(site); err != nil {
-		slog.Warn("Failed to visit: %v", site)
+	if res.StatusCode != 200 {
+		return nil, err
 	}
-	return f
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, nil
 }
 
-func (s Scraper) GetGroupsFromID(site string, id string) map[string]string {
-	f := make(map[string]string)
-
-	s.c.OnHTML("table a[href]", func(e *colly.HTMLElement) {
-		l := e.Attr("href")
-		n := e.Text
-		u, err := url.Parse(l)
-		if err != nil {
-			return
-		}
-		id := u.Query().Get("ID")
-		f[id] = n
-	})
-
-	u, _ := url.Parse(site)
+func getURLWithID(site string, id string) (string, error) {
+	u, err := url.Parse(site)
+	if err != nil {
+		return "", err
+	}
 	q := u.Query()
 	q.Set("ID", id)
 	u.RawQuery = q.Encode()
-	if err := s.c.Visit(u.String()); err != nil {
-		slog.Warn("Failed to visit: %v", site)
+
+	return u.String(), nil
+}
+
+func (s Scraper) GetFields(site string) (map[string]string, error) {
+	f := make(map[string]string)
+
+	doc, err := getDocument(site)
+	if err != nil {
+		return nil, err
 	}
-	return f
+
+	doc.Find("ul.lista-grup a[href]").Each(func(i int, s *goquery.Selection) {
+		l, _ := s.Attr("href")
+		n := s.Text()
+		u, err := url.Parse(l)
+		if err != nil {
+			return
+		}
+		id := u.Query().Get("ID")
+		f[id] = n
+	})
+
+	return f, nil
+}
+
+func (s Scraper) GetGroupsFromID(site string, id string) (map[string]string, error) {
+	g := make(map[string]string)
+
+	u, err := getURLWithID(site, id)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := getDocument(u)
+	if err != nil {
+		return nil, err
+	}
+
+	doc.Find("table a[href]").Each(func(i int, s *goquery.Selection) {
+		l, ok := s.Attr("href")
+		if !ok {
+			return
+		}
+		n := s.Text()
+		u, err := url.Parse(l)
+		if err != nil {
+			return
+		}
+		id := u.Query().Get("ID")
+		g[id] = n
+	})
+
+	return g, nil
 }
 
 func (s Scraper) GetScheduleForID(site string, id string) ([]Entry, error) {
 	var entries []Entry
 	var errs []error
 
-	s.c.OnHTML("#table_details tr:has(td)", func(e *colly.HTMLElement) {
-		date := e.ChildText("td:nth-child(1)")
-		_ = e.ChildText("td:nth-child(2)")
-		group := e.ChildText("td:nth-child(3)")
-		start := e.ChildText("td:nth-child(4)")
-		end := e.ChildText("td:nth-child(5)")
-		subject := e.ChildText("td:nth-child(6)")
-		ClassType := e.ChildText("td:nth-child(7)")
-		teacher := e.ChildText("td:nth-child(8)")
-		classroom := e.ChildText("td:nth-child(9)")
-		ent, err := FromScraper(RawEntry{
+	u, err := getURLWithID(site, id)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := getDocument(u)
+	if err != nil {
+		return nil, err
+	}
+
+	doc.Find("#table_details tr:has(td)").Each(func(i int, s *goquery.Selection) {
+		date := strings.TrimSpace(s.Find("td:nth-child(1)").Text())
+		// WE SKIP THE 2nd child
+		group := strings.TrimSpace(s.Find("td:nth-child(3)").Text())
+		start := strings.TrimSpace(s.Find("td:nth-child(4)").Text())
+		end := strings.TrimSpace(s.Find("td:nth-child(5)").Text())
+		subject := strings.TrimSpace(s.Find("td:nth-child(6)").Text())
+		classType := strings.TrimSpace(s.Find("td:nth-child(7)").Text())
+		teacher := strings.TrimSpace(s.Find("td:nth-child(8)").Text())
+		classroom := strings.TrimSpace(s.Find("td:nth-child(9)").Text())
+		e, err := FromScraper(RawEntry{
 			Group:     group,
 			Start:     start,
 			End:       end,
 			Date:      date,
 			Subject:   subject,
-			ClassType: ClassType,
+			ClassType: classType,
 			Teacher:   teacher,
 			Classroom: classroom,
 		})
 		if err != nil {
 			errs = append(errs, err)
+			log.Print(err)
 		}
-		entries = append(entries, ent)
+		entries = append(entries, e)
 	})
 
-	u, _ := url.Parse(site)
-	q := u.Query()
-	q.Set("ID", id)
-	u.RawQuery = q.Encode()
-	if err := s.c.Visit(u.String()); err != nil {
-		slog.Warn("Failed to visit", site)
-	}
-
-	if len(errs) > 0 {
+	if len(errs) < 0 {
 		return nil, errors.Join(errs...)
 	}
 
 	return entries, nil
+
 }
