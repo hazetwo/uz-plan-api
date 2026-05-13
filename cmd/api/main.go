4356fcc8cdf17ab644f78981fc40cbf181b525c2
haze <92948735+hazetwo@users.noreply.github.com>
2026-05-13 12:11:18 +0200

    add: graceful close and env validation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log/slog"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"uz-plan-api/docs"
 	"uz-plan-api/internal/database"
 	"uz-plan-api/internal/schedule"
@@ -41,7 +46,14 @@ func main() {
 	h := schedule.NewHandler(svc, limiter)
 
 	env := os.Getenv("APP_ENV")
-	if env != "production" {
+
+	switch env {
+	case "prod", "production":
+		// skip registering the docs route
+	case "dev", "development":
+		r.Get("/openapi.yaml", docs.Spec)
+	default:
+		slog.Warn("Unknown APP_ENV value, defaulting to development", "value", env)
 		r.Get("/openapi.yaml", docs.Spec)
 	}
 
@@ -53,12 +65,38 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = ":8080"
 	}
 
-	slog.Info("Server listening", "addr", "http://0.0.0.0:"+port)
-	if err := http.ListenAndServe(":"+port, r); err != nil {
-		slog.Error("Server failed", "err", err)
+	notify, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	defer stop()
+
+	slog.Info("Server listening", "addr", "http://0.0.0.0"+port)
+	server := &http.Server{
+		Addr:    port,
+		Handler: r,
+		BaseContext: func(listener net.Listener) context.Context {
+			return ctx
+		},
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("Server failed", "err", err)
+			os.Exit(1)
+		}
+	}()
+	<-notify.Done()
+
+	shutCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	slog.Info("Shutting down...")
+	err = server.Shutdown(shutCtx)
+	if err != nil {
+		slog.Error("Shutdown failed", "err", err)
 		os.Exit(1)
 	}
+
 }
